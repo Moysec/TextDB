0c110d90538f71bde5244e178d41551e89d9db2d
Metin Emenullahi <metin@mtndesign.net>
2016-03-19 15:02:31 +0200

    DBPATH on init.

--- __init__.py
+++ __init__.py
@@ -1,1 +1,8 @@
+from os import environ as ENV
+from os.path import abspath  as ABSPATH
+from os.path import dirname as DIR
+from sys import path as SYSPATH
+
+ENV['DBPATH'] = ABSPATH(DIR(__file__)) + "/db/"
+
 from dbms import *
